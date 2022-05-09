345c1fb319b6de6ad107ac07b3bf6123975e4e2e
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-09 17:05:40 +0700

    create checkout status screen

--- app/Http/Controllers/API/MidtransController.php
+++ app/Http/Controllers/API/MidtransController.php
@@ -66,4 +66,18 @@ class MidtransController extends Controller
         // simpan transaksi
         $transaction->save();
     }
+
+    public function success(){
+        return view('midtrans.success');
+    }
+
+    public function unfinish(){
+        return view('midtrans.unfinish');
+    }
+
+    public function error(){
+        return view('midtrans.error');
+    }
+
+    
 }
