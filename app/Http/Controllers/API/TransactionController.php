52b1026fb01d1c34a376fb968485358041f4d0a4
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-09 15:53:35 +0700

    add post checkout using midtrans function

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\API;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class TransactionController extends Controller
 {
@@ -65,5 +68,65 @@ class TransactionController extends Controller
         $transaction->update($request->all()); // meng-update semua yang ada di-request
         
         return ResponseFormatter::success($transaction, 'Transaction data successfully updated');
+    }
+
+    public function checkout(Request $request){
+
+        // validasi request dari frontend
+        $request->validate([
+            'food_id' => 'required|exists:food,id',
+            'user_id' => 'required|exists:users,id',
+            'quantity' => 'required',
+            'total' => 'required',
+            'status' => 'required',
+        ]);
+
+        $transaction = Transaction::create([
+            'food_id' => $request->food_id,
+            'user_id' => $request->user_id,
+            'quantity' => $request->quantity,
+            'total' => $request->total,
+            'status' => $request->status,
+            'payment_url' => '', // akan di-update nanti setelah memanggil midtrans
+        ]);
+
+        // konfigurasi MidTrans supaya bisa dipakai
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+        
+        // panggil transaksi yang tadi dibuat, namun kali ini spesifik dengan relasinya
+        $transaction = Transaction::with(['food', 'user'])->find($transaction->id);
+
+        // Membuat Transaksi Midtrans
+        $midtrans = [
+            'transaction_details' => [
+                'order_id' => $transaction->id,
+                'gross_amount' => (int) $transaction->total,
+            ],
+            'customer_details' => [
+                'first_name' => $transaction->user->name,
+                'email' => $transaction->user->email,
+            ],
+            'enable_payments' => ['gopay', 'bank_transfer'],
+            'vtweb' => []
+        ];
+
+        // Memanggil Midtrans
+        try {
+            // ambil halaman payment midtrans
+            $paymentUrl = Snap::createTransaction($midtrans)->redirect_url;
+            
+            // update data di db
+            $transaction->payment_url = $paymentUrl;
+            $transaction->save();
+
+            // Mengembalikan data ke API 
+            return ResponseFormatter::success($transaction, 'Transaction Success');
+
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 'Transaction Failed');
+        }
     }
 }
