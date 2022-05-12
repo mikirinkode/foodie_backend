a507477f82e0bb9436e1d4133f8a39021833b67c
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-12 11:59:43 +0700

    add food controller

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\FoodController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,7 @@ Route::prefix('dashboard')
     ->group(function() {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('users', UserController::class);
+        Route::resource('food', FoodController::class);
     });
 
     
