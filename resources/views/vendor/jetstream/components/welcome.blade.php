965f8411b723e14dc94370e3128ff4dba4ae87be
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-12 11:40:14 +0700

    update navigation

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,25 +1,25 @@
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laravel.com/docs">Food</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/food">Food</a></div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laracasts.com">Transaction</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/transactions">Transaction</a></div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://tailwindcss.com/">Users</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/users">Users</a></div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Profile</div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/user/profile">Profile</a></div>
         </div>
     </div>
 </div>
